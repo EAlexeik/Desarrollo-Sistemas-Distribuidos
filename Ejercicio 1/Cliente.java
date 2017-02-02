155127aed434104f54b2e440daa72b2d28832fa7
Eduardo Alexeik <alexeik93@outlook.com>
2017-02-02 13:03:06 -0600

    Agregado de imports, correccion de errores y copilado de *.java

--- Ejercicio 1/Cliente.java
+++ Ejercicio 1/Cliente.java
@@ -1,9 +1,16 @@
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 public class Cliente{
-	public satic void main(String arg[]) throws UnknownHostException, IOException{
+	public static void main(String arg[]) throws UnknownHostException, IOException{
 		Socket socket=new Socket("127.0.0.1",Servidor.PUERTO);
-		InputStreamReader is=socket.getInputStream();
-		BufferedReader br=new BufferedReader(is);
-		System.out.println(br.readline());
+		InputStream is=socket.getInputStream();
+		BufferedReader br=new BufferedReader(new InputStreamReader(is));
+		System.out.println(br.readLine());
 		br.close();
 		socket.close();
 	}
--- Ejercicio 1/Servidor.java
+++ Ejercicio 1/Servidor.java
@@ -1,3 +1,11 @@
+import java.util.GregorianCalendar;
+import java.text.DateFormat;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.io.IOException;
+
 public class Servidor{
 	public static final int PUERTO=5000;
 	private static String metodoUno(){
@@ -6,11 +14,12 @@ public class Servidor{
 		DateFormat hora=DateFormat.getTimeInstance(DateFormat.SHORT);
 		return hora.format(fecha);
 	}
-	public satic void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException{
 		ServerSocket socketServidor=new ServerSocket(PUERTO);
 		while(true){
 			Socket socketCliente=socketServidor.accept();
 			PrintWriter pw=new PrintWriter(socketCliente.getOutputStream());
+			System.out.println(Servidor.metodoUno());
 			pw.close();
 			socketCliente.close();
 		}

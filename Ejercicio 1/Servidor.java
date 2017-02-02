46c5bd2d7b594a89e1a2f58d304893a3d4fa3de5
Eduardo Alexeik <alexeik93@outlook.com>
2017-02-02 11:47:23 -0600

    Se coloco la definicion del metodoUno y la clase principal Servidor

--- Ejercicio 1/Servidor.java
+++ Ejercicio 1/Servidor.java
@@ -1,4 +1,19 @@
 public class Servidor{
 	public static final int PUERTO=5000;
 	private static String metodoUno(){
-		GregorianCalendar calendario
+		GregorianCalendar calendario=new GregorianCalendar();
+		Date fecha=calendario.getTime();
+		DateFormat hora=DateFormat.getTimeInstance(DateFormat.SHORT);
+		return hora.format(fecha);
+	}
+	public satic void main(String[] args) throws IOException{
+		ServerSocket socketServidor=new ServerSocket(PUERTO);
+		while(true){
+			Socket socketCliente=socketServidor.accept();
+			PrintWriter pw=new PrintWriter(socketCliente.getOutputStream());
+			pw.close();
+			socketCliente.close();
+		}
+	}
+}
+
